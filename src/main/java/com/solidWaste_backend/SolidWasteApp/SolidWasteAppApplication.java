0c5a1a4cfad886e68f8f821b104237c275ea9eda
tarrillollontopVictor <tarrillollontopvictormanuel04@gmail.com>
2024-07-21 18:35:06 -0500

    Creacion del repo remoto

---
+++ src/main/java/com/solidWaste_backend/SolidWasteApp/SolidWasteAppApplication.java
@@ -0,0 +1,13 @@
+package com.solidWaste_backend.SolidWasteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidWasteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidWasteAppApplication.class, args);
+	}
+
+}
